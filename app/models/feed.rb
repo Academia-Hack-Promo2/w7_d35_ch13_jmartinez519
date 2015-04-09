c61a2be41ae0695fd3c3e5e3e6f845e7e2779a2c
Jonathan Martinez <jamm@jmartinezm.org>
2015-04-08 21:29:20 -0430

    Agregado json de Mashable al model feed

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -1,6 +1,6 @@
 class Feed
   include ActiveModel::Model
-  require 'httparty'
+  # require 'httparty'
   require 'date'
 
   class Reddit
@@ -27,12 +27,42 @@ class Feed
     end
   end
 
+
+  class Mashable
+    include HTTParty
+    base_uri 'http://mashable.com/stories.json?limit=10'
+
+    def news
+      response = self.class.get('')
+      return response
+    end
+
+    def format response
+      noticias = []
+      response["hot"].each do |new|
+        res = {}
+        res["author"] = new["author"]  
+        res["title"] = new["title"]
+        res["post_date"] = new["post_date"]
+        res["link"] = new["link"]
+        noticias.push(res)
+      end
+      return noticias
+    end
+  end
+
+  class Digg
+    include HTTParty
+  end
+
   def main
     newsarr = []
     reddit_feed = Reddit.new
-    formato = reddit_feed.format reddit_feed.news
-    newsarr.push(formato)
-    # newsarr.push(reddit_feed.news)
+    formato_reddit = reddit_feed.format reddit_feed.news
+    newsarr.push(formato_reddit)
+    mashable_feed = Mashable.new
+    formato_mashable = mashable_feed.format mashable_feed.news
+    newsarr.push(formato_mashable)
     return newsarr
   end
 end
